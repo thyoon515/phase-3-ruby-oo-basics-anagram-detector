dc9ecead1615fda38ead49752e2917517e6228d4
thyoon515 <thyoon515@gmail.com>
2022-10-04 00:00:46 -0400

    commit tree 7220fe6f07fa1092b88f31a480b44bb3e19d70d8

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(string)
+        string.select do |w|
+            w.chars.sort == word.chars.sort
+        end
+    end
+
+end
